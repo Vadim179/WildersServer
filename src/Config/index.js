43b9003142c28314b25a9d444e8e01e12447bf1c
Vadim Vengriniuc <54371115+Vadim179@users.noreply.github.com>
2022-02-02 18:43:30 +0200

    Resolve paths

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
-const CorsConfig = require('./Cors.Config')
+const path = require('path')
+
+const KEY_PATH = path.resolve(__dirname, 'key.pem')
+const CERT_PATH = path.resolve(__dirname, 'cert.pem')
 
 const ServerConfig = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync(KEY_PATH),
+  cert: fs.readFileSync(CERT_PATH),
 }
 
-module.exports = {
-  CorsConfig,
-  ServerConfig,
-}
+const CorsConfig = require('./Cors.Config')
+module.exports = { CorsConfig, ServerConfig }
